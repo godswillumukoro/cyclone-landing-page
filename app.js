a5851d855f72f90b1ece6db3fdce9e56f3335917
Godswill Umukoro <umukoro6@gmail.com>
2022-10-28 11:50:28 +0100

    modal for desktop screens

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// alert('Hello')
+const modal = document.getElementById("modal")
+const closeModal = document.getElementById("close")
+const joinWaitlist = document.querySelectorAll(".join-waitlist")
+
+joinWaitlist.forEach(btn =>
+    btn.addEventListener("click", () => {
+        modal.showModal()
+    }))
+
+closeModal.addEventListener("click", () => {
+    modal.close()
+})
