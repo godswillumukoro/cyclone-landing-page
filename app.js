3c4973b5f40b9a7dc680cb0095423093678880c6
Godswill Umukoro <umukoro6@gmail.com>
2022-11-03 10:05:08 +0100

    disable zoom on iphones

--- app.js
+++ app.js
@@ -3,30 +3,45 @@ const closeModal = document.getElementById("close")
 const joinWaitlist = document.querySelectorAll(".join-waitlist")
 
 joinWaitlist.forEach(btn =>
-    btn.addEventListener("click", () => {
-        modal.showModal()
-    }))
+  btn.addEventListener("click", () => {
+    modal.showModal()
+  }))
 
 closeModal.addEventListener("click", () => {
-    modal.close()
+  modal.close()
 })
 
 // Netlify Forms
 const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-  };
-  
-  document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
+
+
+// This prevents iOS zoom on focused form elements with font size smaller than 16px.
+// iOS 10+ allows pinch-zoom even if viewport is unscalaeable.
+// We feature detect those browsers and apply additional rules.
+if (
+  window.CSS?.supports?.(
+    '(font:-apple-system-body) and (-webkit-touch-callout:none) and (-webkit-tap-highlight-color:hotpink)'
+  )
+) {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  if (viewport !== null) {
+    viewport.content = `${viewport.content},user-scalable=no`;
+  }
+}
